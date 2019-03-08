d5946630ce3ae6b206e9b2da6ec2586c8358937c
maxjamin <maxjamin@gmail.com>
2019-03-08 14:40:34 -0700

    Adding first page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const pool = new Pool()
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
