09c4350fbb160077446ba9411e7e116d8c39042f
maxjamin <maxjamin@gmail.com>
2019-03-08 12:47:34 -0700

    Adding first page

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ express()
   		console.log("loginUser request");
 
   		var par = {weight: 45};
-  		res.render('/main', par)
+  		res.render('pages/main', par)
   })
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
