afa159a6ef5d475ae7530c1ab45a4c1537fb5dc0
maxjamin <maxjamin@gmail.com>
2019-03-08 15:21:51 -0700

    Adding first page

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ express()
       		const client = await pool.connect()
       		const result = await client.query('SELECT * FROM Customers');
       		const results = { 'results': (result) ? result.rows : null};
-      		res.render('pages/db', results );
+
+      		res.render('pages/main', res.rows[0]);
       		client.release();
+
     	} catch (err) {
       		console.error(err);
       		res.send("Error " + err);
     	}  		
-
-  		res.render('pages/main', res.rows[0]);
 
   })
 
