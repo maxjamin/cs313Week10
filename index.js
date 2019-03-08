e268410132f7d5d36474ac4e3b540fab106e041a
maxjamin <maxjamin@gmail.com>
2019-03-08 14:33:35 -0700

    Adding first page

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-
+const { Pool } = require('pg')
+const connectionString = process.env.DATABASE_URL
+const pool = new Pool({connectionString: connectionString})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
