bd0650769b4a5295841bcf10795b2fed36592ea7
maxjamin <maxjamin@gmail.com>
2019-03-08 16:19:56 -0700

    Adding first page

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function getPerson(request, response) {
 			response.status(500).json({success: false, data: error});
 		} else {
 			var person = result[0];
-			response.status(200).json(result[0]);
+			response.json(result[0]);
 
 		}
 	});
